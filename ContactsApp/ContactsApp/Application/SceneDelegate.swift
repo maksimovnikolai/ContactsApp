4394ab5ad8473848735fe549a66214ccdd41da71
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-02 17:53:55 +0300

    + стартовый экран - contact list viewcontroller

--- ContactsApp/ContactsApp/Application/SceneDelegate.swift
+++ ContactsApp/ContactsApp/Application/SceneDelegate.swift
@@ -12,7 +12,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let rootVC = UINavigationController(rootViewController: ContactListViewController())
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
     }
 }
 
---
+++ ContactsApp/ContactsApp/Controllers/ContactListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ContactListViewController.swift
+//  ContactsApp
+//
+//  Created by Nikolai Maksimov on 02.01.2024.
+//
+
+import UIKit
+
+final class ContactListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
