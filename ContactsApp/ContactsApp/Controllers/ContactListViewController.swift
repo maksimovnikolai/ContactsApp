d9646d3d2ea797313feff428fc3f9173d1200bd3
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-02 18:06:19 +0300

    + табличное представление

--- ContactsApp/ContactsApp/Controllers/ContactListViewController.swift
+++ ContactsApp/ContactsApp/Controllers/ContactListViewController.swift
@@ -9,8 +9,43 @@ import UIKit
 
 final class ContactListViewController: UIViewController {
     
+    // MARK: Private properties
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        commonInit()
+    }
+}
+
+// MARK: - Private methods
+extension ContactListViewController {
+    
+    private func commonInit() {
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "contact")
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ContactListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contact", for: indexPath)
+        return cell
     }
 }
